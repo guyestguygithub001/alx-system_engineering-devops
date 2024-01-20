8bef1d04ee1fbea8a5e228c599efaee3c21784d8
guyestguygithub001 <danielene050@gmail.com>
2024-01-20 01:50:24 -0800

    commit for change

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 #!/usr/bin/pup
-# Installs  a very  specified version of flask (2.1.0)
+# Installs a very specified version of flask (2.1.0)
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
