be8b3afb801dbb07659e06664f57cf01620ed609
guyestguygithub001 <danielene050@gmail.com>
2024-01-21 02:46:21 -0800

    Task Four

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,27 @@
-#!/usr/bin/env bash
-# For using puppet code to effect and add changes to config file
+# 100-puppet_ssh_config.pp
+# This file configures the SSH client to use the private key ~/.ssh/school
+# and to refuse to authenticate using a password
 
-file_line { 'Turn off passwd auth':
-  path  => '/etc/ssh/sshd_config',
-  line  => 'PasswordAuthentication no',
-  match => '^#?PasswordAuthentication',
+# Ensure that the line 'IdentityFile ~/.ssh/school' is present in the file
+file_line { 'Declare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
 }
 
-file_line { 'Declare identity file':
-  path  => '/etc/ssh/ssh_config',
-  line  => 'IdentityFile ~/.ssh/school',
-  match => '^#?IdentityFile',
+# Ensure that the line 'PasswordAuthentication no' is present in the file
+file_line { 'Turn off password auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
+
+# Ensure that any line that contains 'PasswordAuthentication yes' is removed from the file
+file_line { 'Remove password auth':
+  ensure            => absent,
+  path              => '/etc/ssh/ssh_config',
+  match             => '.*PasswordAuthentication yes.*',
+  multiple          => true,
+  match_for_absence => true,
 }
 
